ec2f48ea579905562e1b0c83b4e0ed53cc33db05
wkwukai <2074153271@qq.com>
2019-03-05 22:11:55 +0800

    powerWithunsignedExponent

---
+++ powerWithunsignedExponent.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+
+
+bool invalideInput = flase;
+bool equal();
+double powerWithunsignedExponent();
+
+void Test(int base,unsigned int exponent,double expected result,bool expected flag){
+
+}
+
+void Test1(){
+
+}
+
+int main(){
+   Test1();
+   return 0;
+}
